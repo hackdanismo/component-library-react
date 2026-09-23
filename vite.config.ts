b4b8997017a056f2ac623cade273fd425431a174
Dan Jackson <hackdanismo@gmail.com>
2026-09-23 09:56:03 +0100

    feat: setup Storybook for the initial Button component and configure Storybook to remove default example components from being rendered

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,49 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
 import { fileURLToPath, URL } from "node:url";
+import path from 'node:path';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+import { playwright } from '@vitest/browser-playwright';
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    dts({
-      insertTypesEntry: true,
-    }),
-  ],
-
+  plugins: [react(), tailwindcss(), dts({
+    insertTypesEntry: true
+  })],
   build: {
     lib: {
       entry: fileURLToPath(new URL("./src/index.ts", import.meta.url)),
       formats: ["es", "cjs"],
-      fileName: (format) => `index.${format}.js`,
+      fileName: format => `index.${format}.js`
     },
-
     rollupOptions: {
-      external: ["react", "react-dom"],
-    },
+      external: ["react", "react-dom"]
+    }
   },
+  test: {
+    projects: [{
+      extends: true,
+      plugins: [
+      // The plugin will run tests for the stories defined in your Storybook config
+      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+      storybookTest({
+        configDir: path.join(dirname, '.storybook')
+      })],
+      test: {
+        name: 'storybook',
+        browser: {
+          enabled: true,
+          headless: true,
+          provider: playwright({}),
+          instances: [{
+            browser: 'chromium'
+          }]
+        }
+      }
+    }]
+  }
 });
