675a26db0d4097a1be672b7e67d522688a823252
Dan Jackson <hackdanismo@gmail.com>
2026-09-23 11:23:25 +0100

    chore: update the index.ts file to import any missing components and also fix typo

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,101 @@
 import "./styles.css";
 
 // Components
+// Allows consumers of the NPM package can import individual components.
+
+// Button
 export { Button } from "./components/Button/Button";
 export type { ButtonProps } from "./components/Button/Button";
+
+// Section Intro
 export { SectionIntro } from "./components/SectionIntro/SectionIntro";
 export type { SectionIntroProps } from "./components/SectionIntro/SectionIntro";
+
+// Card
 export { Card } from "./components/Card/Card";
-export type { CardProps CardAction } from "./components/Card/Card";
+export type {
+  CardProps,
+  CardAction,
+} from "./components/Card/Card";
+
+// Card Container
 export { CardContainer } from "./components/CardContainer/CardContainer";
-export type { CardContainerProps } from "./components/CardContainer/CardContainer";
+export type {
+  CardContainerProps,
+} from "./components/CardContainer/CardContainer";
+
+// Card Header
 export { CardHeader } from "./components/CardHeader/CardHeader";
-export type { CardHeaderProps } from "./components/CardHeader/CardHeader";
+export type {
+  CardHeaderProps,
+} from "./components/CardHeader/CardHeader";
+
+// Icons
 export { WifiIcon } from "./components/WifiIcon/WifiIcon";
+
+// Feature Item
+export { FeatureItem } from "./components/FeatureItem/FeatureItem";
+export type {
+  FeatureItemProps,
+} from "./components/FeatureItem/FeatureItem";
+
+// Feature List
 export { FeatureList } from "./components/FeatureList/FeatureList";
-export type { FeatureListProps, Feature } from "./components/FeatureList/FeatureList";
+export type {
+  FeatureListProps,
+  Feature,
+} from "./components/FeatureList/FeatureList";
+
+// Form Input
+export { FormInput } from "./components/FormInput/FormInput";
+export type {
+  FormInputProps,
+} from "./components/FormInput/FormInput";
+
+// Business Details Form
+export { BusinessDetailsForm } from "./components/BusinessDetailsForm/BusinessDetailsForm";
+export type {
+  BusinessDetails,
+  BusinessDetailsFormProps,
+} from "./components/BusinessDetailsForm/BusinessDetailsForm";
+
+// Basket Item
+export { BasketItem } from "./components/BasketItem/BasketItem";
+export type {
+  BasketItemProps,
+} from "./components/BasketItem/BasketItem";
+
+// Payment Method Selector
+export { PaymentMethodSelector } from "./components/PaymentMethodSelector/PaymentMethodSelector";
+export type {
+  PaymentMethod,
+  PaymentMethodSelectorProps,
+} from "./components/PaymentMethodSelector/PaymentMethodSelector";
+
+// Basket Summary
+export { BasketSummary } from "./components/BasketSummary/BasketSummary";
+export type {
+  BasketSummaryProps,
+} from "./components/BasketSummary/BasketSummary";
+
+// Checkout
+export { Checkout } from "./components/Checkout/Checkout";
+export type {
+  CheckoutProps,
+} from "./components/Checkout/Checkout";
+
+// Checkout Header
+export { CheckoutHeader } from "./components/Checkout/CheckoutHeader";
+export type {
+  CheckoutHeaderProps,
+} from "./components/Checkout/CheckoutHeader";
+
+// Purchase Flow
 export { PurchaseFlow } from "./components/PurchaseFlow/PurchaseFlow";
-export type { PurchaseFlowProps, PurchaseFlowPackage } from "./components/PurchaseFlow/PurchaseFlow";
+export type {
+  PurchaseFlowProps,
+  PurchaseFlowPackage,
+} from "./components/PurchaseFlow/PurchaseFlow";
+
+// Shared Types
+export type { Package } from "./types/Package";
